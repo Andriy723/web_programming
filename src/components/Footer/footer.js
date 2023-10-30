7399879e53344bd048e64153ddbd7d343467d91f
Andriy723 <113835398+Andriy723@users.noreply.github.com>
2023-10-31 00:40:23 +0200

    Adding 6 lab to git with react and view page

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,14 @@
+function Footer() {
+    return (
+        <div className="footer">
+            <nav className="footer__nav">
+                <p>
+                    2023 IoT ©Copyright all rights reserved.
+                    Done by Plish Andriy
+                </p>
+            </nav>
+        </div>
+    );
+}
+
+export default Footer;
